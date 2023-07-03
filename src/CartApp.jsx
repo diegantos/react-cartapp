7340a3cc846a368fd864e7fff694470d76ae4ae1
Diegantos <diegantos8@gmail.com>
2023-07-03 17:24:02 +0200

    new structure

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,8 +1,67 @@
+import { useEffect, useState } from "react"
+import { getProducts } from "./services/productService"
 
 export const CartApp = () => {
-    return(
-        <>
-            <div className="CartApp">Hello from CartApp Component!</div>
-        </>
-    )
+
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        setProducts(getProducts())
+    }, [])
+
+    return (
+      <>
+        <div className="container">
+
+            <h3>Cart App</h3>
+            <div className="row">
+                { products.map((prod)=>(
+                    <div key={prod.id} className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{prod.name}</h5>
+                                <p className="card-text">{prod.description}</p>
+                                <p className="card-text">{prod.price}</p>
+                                <button className="btn btn-primary">Añadir al carrito</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            
+          <div className="my-4 w-50">
+            <h3>Carro de la compra</h3>
+            <table className="table table-hover table-striped">
+              <thead>
+                <tr>
+                  <th>Producto</th>
+                  <th>Precio</th>
+                  <th>Cantidad</th>
+                  <th>Total</th>
+                  <th>Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Nombre</td>
+                  <td>Precio</td>
+                  <td>cantidad</td>
+                  <td>total</td>
+                  <td>eliminar</td>
+                </tr>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan={3} className="text-end fw-bold">
+                    Total
+                  </td>
+                  <td colSpan={2} className="text-start fw-bold">
+                    12345
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </>
+    );
 }
